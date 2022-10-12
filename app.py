532a47fd9888d45349074aa1918bd1bb989830f6
StartDiggin <dimensions122@gmail.com>
2022-10-12 14:15:44 -0400

    added recipes page, json folder, and dictionary py folder

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+import flask
+from flask import Flask, render_template, request
+
+# import json
 
 app = Flask(__name__)
 
@@ -6,6 +9,17 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('index.html')
+
+
+@app.route("/recipe")
+def recipe():
+    return render_template('recipe.html')
+
+
+@app.route("/recipes", methods=['GET'])
+def recipes():
+
+    return render_template('recipes.html')
 
 
 if __name__ == '__name__':
---
+++ data/recipes.py
@@ -0,0 +1,56 @@
+recipes = {
+  "recipe-1": {
+    "recipe_id": 1,
+    "name": "Pumpkin Pie Espresso Martinis",
+    "ingredients": [
+      "Double Shot of Espresso",
+      "1 Tablespoon Brown Sugar (Swerve)",
+      "4 Shots of Vodka",
+      "1.5 Shots of Pumpkin Creamer"
+    ],
+    "instructions": [
+      "First, set your glasses in the freezer to chill.",
+      "Brew your double espresso pod.",
+      "Next, stir in the brown sugar to the hot espresso to mix well.",
+      "Add ice to your shaker followed by all your ingredients.",
+      "Shake well!",
+      "Pour into your two martini glasses, top with pumpkin pie spice and enjoy!"
+    ]
+  },
+  "recipe-2": {
+    "recipe_id": 2,
+    "name": "Pumpkin Pie Espresso Martinis",
+    "ingredients": [
+      "Double Shot of Espresso",
+      "1 Tablespoon Brown Sugar (Swerve)",
+      "4 Shots of Vodka",
+      "1.5 Shots of Pumpkin Creamer"
+    ],
+    "instructions": [
+      "First, set your glasses in the freezer to chill.",
+      "Brew your double espresso pod.",
+      "Next, stir in the brown sugar to the hot espresso to mix well.",
+      "Add ice to your shaker followed by all your ingredients.",
+      "Shake well!",
+      "Pour into your two martini glasses, top with pumpkin pie spice and enjoy!"
+    ]
+  },
+  "recipe-3": {
+    "recipe_id": 3,
+    "name": "Pumpkin Pie Espresso Martinis",
+    "ingredients": [
+      "Double Shot of Espresso",
+      "1 Tablespoon Brown Sugar (Swerve)",
+      "4 Shots of Vodka",
+      "1.5 Shots of Pumpkin Creamer"
+    ],
+    "instructions": [
+      "First, set your glasses in the freezer to chill.",
+      "Brew your double espresso pod.",
+      "Next, stir in the brown sugar to the hot espresso to mix well.",
+      "Add ice to your shaker followed by all your ingredients.",
+      "Shake well!",
+      "Pour into your two martini glasses, top with pumpkin pie spice and enjoy!"
+    ]
+  }
+}
